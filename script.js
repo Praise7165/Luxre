434f04be695ea13aae7f92944362e8db519d7cc6
Omolade Lekan <omoladelekan99@gmail.com>
2023-09-06 06:41:59 +0100

    Continu to make changes to the CSS

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let projects = document.querySelector(".projects");
 let testimonials = document.querySelector(".testimonial");
 
 for (let project of projectList) {
+
+    let projectDate = new Date(project.DateCompleted);
+
+
     let projectCard = document.createElement("div");
     let projectDetails = document.createElement("div");
     let heading = document.createElement("h3");
@@ -14,7 +18,8 @@ for (let project of projectList) {
 
     heading.innerText = project.Name;
     location.innerText = project.Location;
-    dateCompleted.innerText = project.DateCompleted;
+
+    dateCompleted.innerText = projectDate.getFullYear();
     type.innerText = project.Type;
     bgImage.style.backgroundImage = `url("${project.CoverImage}")`;
     bgImage.style.height = "320px";
@@ -30,14 +35,22 @@ for (let project of projectList) {
 
     projectCard.appendChild(bgImage);
     projectCard.appendChild(projectDetails);
+
+    heading.classList.add("project-name");
+
+    location.classList.add("project-tag");
+    dateCompleted.classList.add("project-tag");
+    type.classList.add("project-tag");
+
+    tagWrapper.classList.add("tag-wrapper");
+    projectDetails.classList.add("project-details");
+    bgImage.classList.add("project-image");
     
-}
 
 
-for (let project of projectList) {
 
-    let projectDate = new Date(project.DateCompleted);
 
+    
     if (projectDate.getFullYear() < 2022) {
         continue;
     } else {
@@ -54,4 +67,5 @@ for (let project of projectList) {
         testimonials.appendChild(projectName);
         testimonials.appendChild(testimonialCard);
     }
+    
 }
